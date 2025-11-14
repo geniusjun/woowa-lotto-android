db731625678800277dc9354393fc2418284dc12c
geniusjun <geniusjun4663@gmail.com>
2025-11-14 20:54:32 +0900

    Feat(AuthApi): 로그아웃 로직 추가

--- app/src/main/java/com/geniusjun/lotto/data/api/AuthApi.kt
+++ app/src/main/java/com/geniusjun/lotto/data/api/AuthApi.kt
@@ -4,11 +4,15 @@ import com.geniusjun.lotto.data.model.ApiResponse
 import com.geniusjun.lotto.data.model.GoogleLoginRequest
 import com.geniusjun.lotto.data.model.LoginResponse
 import retrofit2.http.Body
+import retrofit2.http.DELETE
 import retrofit2.http.POST
 
 interface AuthApi {
     @POST("api/auth/google")
     suspend fun login(@Body request: GoogleLoginRequest): ApiResponse<LoginResponse>
+    
+    @DELETE("api/auth/logout")
+    suspend fun logout(): ApiResponse<String>
 }
 
 
