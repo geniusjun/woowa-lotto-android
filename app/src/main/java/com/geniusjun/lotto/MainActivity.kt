1a74541e2f695c8a084619dd35f1fba970bf00f1
geniusjun <geniusjun4663@gmail.com>
2025-11-14 09:44:47 +0900

    Feat(MainActivity): 메인함수에 flow 추가

--- app/src/main/java/com/geniusjun/lotto/MainActivity.kt
+++ app/src/main/java/com/geniusjun/lotto/MainActivity.kt
@@ -3,7 +3,9 @@ package com.geniusjun.lotto
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.runtime.*
 import com.geniusjun.lotto.ui.screen.LottoFortuneApp
+import com.geniusjun.lotto.ui.screen.OnboardingScreen
 import com.geniusjun.lotto.ui.theme.LottoTheme
 
 class MainActivity : ComponentActivity() {
@@ -11,8 +13,25 @@ class MainActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
         setContent {
             LottoTheme {
-                LottoFortuneApp()
+                AppContent()
             }
         }
     }
 }
+
+@Composable
+private fun AppContent() {
+    var isLoggedIn by remember { mutableStateOf(false) }
+    
+    if (isLoggedIn) {
+        LottoFortuneApp()
+    } else {
+        OnboardingScreen(
+            onGoogleSignInClick = {
+                // TODO: 구글 로그인 구현 필요
+                isLoggedIn = true
+            }
+        )
+    }
+}
+
