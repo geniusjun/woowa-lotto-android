079e5144435228e10fa45ccfbfa91e53b7438c4d
geniusjun <geniusjun4663@gmail.com>
2025-11-14 15:46:09 +0900

    Refactor(MainActivity): 메인 함수 흐름 수정

--- app/src/main/java/com/geniusjun/lotto/MainActivity.kt
+++ app/src/main/java/com/geniusjun/lotto/MainActivity.kt
@@ -4,8 +4,11 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.runtime.*
+import androidx.compose.ui.platform.LocalContext
+import com.geniusjun.lotto.auth.GoogleSignInManager
+import com.geniusjun.lotto.data.network.TokenProvider
+import com.geniusjun.lotto.ui.screen.AuthScreen
 import com.geniusjun.lotto.ui.screen.LottoFortuneApp
-import com.geniusjun.lotto.ui.screen.OnboardingScreen
 import com.geniusjun.lotto.ui.theme.LottoTheme
 
 class MainActivity : ComponentActivity() {
@@ -21,14 +24,18 @@ class MainActivity : ComponentActivity() {
 
 @Composable
 private fun AppContent() {
-    var isLoggedIn by remember { mutableStateOf(false) }
+    val context = LocalContext.current
+    val signInManager = remember {
+        GoogleSignInManager(context, TokenProvider(context))
+    }
+    
+    var isLoggedIn by remember { mutableStateOf(signInManager.isSignedIn()) }
     
     if (isLoggedIn) {
         LottoFortuneApp()
     } else {
-        OnboardingScreen(
-            onGoogleSignInClick = {
-                // TODO: 구글 로그인 구현 필요
+        AuthScreen(
+            onLoginSuccess = {
                 isLoggedIn = true
             }
         )
