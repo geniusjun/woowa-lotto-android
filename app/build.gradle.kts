da5d41fda8651f53576424008811f40883e630bf
geniusjun <geniusjun4663@gmail.com>
2025-11-14 09:54:01 +0900

    Infra(build.gradle.kts): 구글 로그인을 위한 의존성 추가

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -57,4 +57,7 @@ dependencies {
     androidTestImplementation(libs.androidx.compose.ui.test.junit4)
     debugImplementation(libs.androidx.compose.ui.tooling)
     debugImplementation(libs.androidx.compose.ui.test.manifest)
+    
+    // Google Sign-In
+    implementation(libs.google.play.services.auth)
 }
