c9a2f20f3ad04f74695342d837907749d882cb7e
geniusjun <geniusjun4663@gmail.com>
2025-11-15 14:31:24 +0900

    Feat(LottoApi): 랜덤 로또 구매, 회원 잔액 조회 api 엔드포인트 추가

--- app/src/main/java/com/geniusjun/lotto/data/api/LottoApi.kt
+++ app/src/main/java/com/geniusjun/lotto/data/api/LottoApi.kt
@@ -1,8 +1,11 @@
 package com.geniusjun.lotto.data.api
 
 import com.geniusjun.lotto.data.model.ApiResponse
+import com.geniusjun.lotto.data.model.BalanceResponse
+import com.geniusjun.lotto.data.model.LottoDrawResponse
 import com.geniusjun.lotto.data.model.WinningNumbersResponse
 import retrofit2.http.GET
+import retrofit2.http.POST
 
 interface LottoApi {
     /**
@@ -10,5 +13,17 @@ interface LottoApi {
      */
     @GET("api/winning/latest")
     suspend fun getLatestWinningNumbers(): ApiResponse<WinningNumbersResponse>
+    
+    /**
+     * 랜덤 로또 구매
+     */
+    @POST("api/lotto/draw")
+    suspend fun drawLotto(): ApiResponse<LottoDrawResponse>
+    
+    /**
+     * 회원 잔액 조회
+     */
+    @GET("api/member/balance")
+    suspend fun getBalance(): ApiResponse<BalanceResponse>
 }
 
