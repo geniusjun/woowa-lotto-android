c549281618254ef4f197a2697a6188cd58cf117d
geniusjun <geniusjun4663@gmail.com>
2025-11-15 14:29:38 +0900

    Feat(DialogState): 다이얼로그 표시 상태를 관리하는 데이터 클래스 구현

---
+++ app/src/main/java/com/geniusjun/lotto/ui/screen/DialogState.kt
@@ -0,0 +1,31 @@
+package com.geniusjun.lotto.ui.screen
+
+import com.geniusjun.lotto.data.model.LottoDrawResponse
+
+/**
+ * 다이얼로그 표시 상태를 관리하는 데이터 클래스
+ */
+data class DialogState(
+    val showLotto: Boolean = false,
+    val showFortune: Boolean = false,
+    val showFortuneAlready: Boolean = false,
+    val showNoMoney: Boolean = false,
+    val lottoDrawResult: LottoDrawResponse? = null
+) {
+    fun showLottoDialog(result: LottoDrawResponse) = copy(
+        showLotto = true,
+        lottoDrawResult = result
+    )
+    
+    fun showFortuneDialog() = copy(showFortune = true)
+    fun showFortuneAlreadyDialog() = copy(showFortuneAlready = true)
+    fun showNoMoneyDialog() = copy(showNoMoney = true)
+    
+    fun closeAll() = copy(
+        showLotto = false,
+        showFortune = false,
+        showFortuneAlready = false,
+        showNoMoney = false
+    )
+}
+
